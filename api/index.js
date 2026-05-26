e299665f734d9d9dc2f1d1577c633f994cc2f941
Yashika Sapra <yashika2865@gmail.com>
2026-05-26 17:06:31 +0530

    now oush

--- api/index.js
+++ api/index.js
@@ -1,22 +1,24 @@
-const fs = require('fs-extra');
-const path = require('path');
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+})
 
 module.exports = async (req, res) => {
-    const dbPath = path.resolve(process.cwd(), 'api', 'db.json');
-
     if (req.method === 'GET') {
         try {
-            const data = await fs.readJson(dbPath);
+            const data = await redis.get('user_data');
             res.status(200).json(data);
         } catch (error) {
-            res.status(500).json({ message: 'Error reading data', error });
+            res.status(500).json({ message: 'Error reading data from Upstash Redis', error });
         }
     } else if (req.method === 'POST') {
         try {
-            await fs.writeJson(dbPath, req.body, { spaces: 2 });
+            await redis.set('user_data', req.body);
             res.status(200).json({ message: 'Data updated successfully' });
         } catch (error) {
-            res.status(500).json({ message: 'Error writing data', error });
+            res.status(500).json({ message: 'Error writing data to Upstash Redis', error });
         }
     } else {
         res.status(405).json({ message: 'Method not allowed' });
